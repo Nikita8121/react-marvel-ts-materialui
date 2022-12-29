b1e507135377582fbada6553f56e3da0e273bcf5
Nikita8121 <natanalegarh@gmail.com>
2022-12-29 12:41:21 +0200

    init structure

---
+++ src/hooks/http.hook.ts
@@ -0,0 +1,37 @@
+import { useState, useCallback } from "react";
+import axios, { AxiosRequestConfig, AxiosResponse} from 'axios';
+
+export const useHttp = () => {
+  const [loading, setLoading] = useState<boolean | null>(false);
+  const [error, setError] = useState<boolean | null>(null);
+
+  const request = useCallback(
+    async (
+      url: string,
+      method: string = "GET",
+      body = null,
+      headers = { "Content-Type": "application/json" }
+    ) => {
+      setLoading(true);
+
+      try {
+        const config : AxiosRequestConfig = { method, url, data:body, headers }
+        const response : AxiosResponse = await axios(config);
+
+        const data = response.data;
+
+        setLoading(false);
+        return data;
+      } catch (e : any) {
+        setLoading(false);
+        setError(e.message);
+        throw e;
+      }
+    },
+    []
+  );
+
+  const clearError = useCallback(() => setError(null), []);
+
+  return { loading, request, error, clearError };
+};
